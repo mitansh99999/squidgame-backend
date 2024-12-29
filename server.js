3fad8d4dab1f7b3b1aea1907d82a59203c3ac27c
mitansh99999 <mitanshdhiman86@gmail.com>
2024-12-29 08:07:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const commentRoutes = require('./routes/comments');
+const commentRoutes = require('./api/comments');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
